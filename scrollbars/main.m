987fa1e4477b5841d921b6776813a4f14c06fa22
Brandon LeBlanc <demosdemon@gmail.com>
2016-11-05 10:51:16 -0500

    implement

--- scrollbars/main.m
+++ scrollbars/main.m
@@ -8,10 +8,53 @@
 
 #import <Foundation/Foundation.h>
 
+void usage(NSString * name);
+void changeScrollBarAction(CFStringRef setting);
+
 int main(int argc, const char * argv[]) {
   @autoreleasepool {
-      // insert code here...
-      NSLog(@"Hello, World!");
+    NSString * name = [NSString stringWithUTF8String:argv[0]];
+
+    if (argc != 2) {
+      usage(name);
+      return 1;
+    }
+
+    NSSet<NSString *> * settings = [NSSet setWithObjects:
+                                    @"Automatic",
+                                    @"WhenScrolling",
+                                    @"Always",
+                                    nil];
+
+    NSString * parameter = [NSString stringWithUTF8String:argv[1]];
+    if ([parameter characterAtIndex:0] == '-') {
+      parameter = [parameter substringFromIndex:1];
+    }
+
+    if ([settings containsObject:parameter]) {
+      changeScrollBarAction((__bridge CFStringRef)(parameter));
+    } else {
+      usage(name);
+    }
   }
-    return 0;
+  return 0;
+}
+
+void usage(NSString * name) {
+  NSLog(@"Usage: %@ [ -Automatic | -WhenScrolling | -Always ]", name);
+}
+
+void changeScrollBarAction(CFStringRef setting) {
+  CFPreferencesSetValue(CFSTR("AppleShowScrollBars"),
+                        setting,
+                        kCFPreferencesAnyApplication,
+                        kCFPreferencesCurrentUser,
+                        kCFPreferencesAnyHost);
+
+  CFPreferencesSynchronize(kCFPreferencesAnyApplication,
+                           kCFPreferencesCurrentUser,
+                           kCFPreferencesAnyHost);
+
+  [[NSDistributedNotificationCenter defaultCenter] postNotificationName:@"AppleShowScrollBarsSettingChanged"
+                                                                 object:nil];
 }
