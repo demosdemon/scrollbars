de78b09264f35e3f7c414f0e3b57993dc52e953c
Brandon LeBlanc <demosdemon@gmail.com>
2016-11-05 16:08:47 -0500

    DRY the settings in usage, clean up the name in usage line

--- scrollbars/main.m
+++ scrollbars/main.m
@@ -8,40 +8,56 @@
 
 #import <Foundation/Foundation.h>
 
-void usage(NSString * name);
+void usage(NSString * name) __attribute__((noreturn));
 void changeScrollBarAction(CFStringRef setting);
+
+const NSString * settings[] = {
+  @"Automatic",
+  @"WhenScrolling",
+  @"Always"
+};
+
+const size_t settingsCount = sizeof(settings)/sizeof(NSString *);
 
 int main(int argc, const char * argv[]) {
   @autoreleasepool {
     NSString * name = [NSString stringWithUTF8String:argv[0]];
 
-    if (argc != 2) {
+    if (argc != 2)
       usage(name);
-      return 1;
-    }
 
-    NSSet<NSString *> * settings = [NSSet setWithObjects:
-                                    @"Automatic",
-                                    @"WhenScrolling",
-                                    @"Always",
-                                    nil];
+    NSSet<NSString *> * validSettings = [NSSet setWithObjects:settings count:settingsCount];
 
     NSString * parameter = [NSString stringWithUTF8String:argv[1]];
     if ([parameter characterAtIndex:0] == '-') {
       parameter = [parameter substringFromIndex:1];
     }
 
-    if ([settings containsObject:parameter]) {
+    if ([validSettings containsObject:parameter])
       changeScrollBarAction((__bridge CFStringRef)(parameter));
-    } else {
+    else
       usage(name);
-    }
   }
+
   return 0;
 }
 
 void usage(NSString * name) {
-  NSLog(@"Usage: %@ [ -Automatic | -WhenScrolling | -Always ]", name);
+  NSRange loc = [name rangeOfString:@"/" options:NSBackwardsSearch];
+  if (loc.length > 0)
+    name = [name substringFromIndex:loc.location + loc.length];
+
+  NSMutableString * body = NSMutableString.string;
+
+  for (int idx = 0; idx < settingsCount; ++idx) {
+    if (idx != 0)
+      [body appendString:@" | "];
+
+    [body appendFormat:@"-%@", settings[idx]];
+  }
+
+  NSLog(@"Usage: %@ [ %@ ]", name, body);
+  exit(1);
 }
 
 void changeScrollBarAction(CFStringRef setting) {
